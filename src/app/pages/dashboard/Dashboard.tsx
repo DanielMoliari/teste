53d48a4561bd0283bee36273df79453c4482fa6a
Daniel Barbosa <daniel.barbosa@a4pm.com.br>
2024-05-15 15:53:04 -0300

    Final da aula 11

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,18 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+
 export const Dashboard = () => {
-    return(
-        <p>Ola Dash</p>
-    )
-}
+  const counterRef = useRef({ counter: 0 });
+
+  return (
+    <div>
+      <p>Dashboard</p>
+
+      <p>Contador: {counterRef.current.counter}</p>
+
+      <button onClick={() => counterRef.current.counter++}>Somar</button>
+
+      <Link to="/entrar">Login</Link>
+    </div>
+  );
+};
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
-  const handleEntrar = () => {
+  const emailLength = useMemo(() => {
+    console.log("Executou");
+    return email.length * 1000;
+  }, [email.length]);
+
+  const handleEntrar = useCallback(() => {
     console.log(email);
     console.log(senha);
-  };
+  }, [email, senha]);
+
   return (
     <div>
       <form>
+        <p>Quantidade de caracteres no email: {emailLength}</p>
         <label>
           <span>Email</span>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" ? inputPasswordRef.current?.focus() : undefined
+            }
+          />
         </label>
         <label>
           <span>Senha</span>
           <input
+            ref={inputPasswordRef}
             type="password"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
